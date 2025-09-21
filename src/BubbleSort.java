27c4b06a38a38abb0df2779bee5a284dfa5c4bbe
sn-varsha <varshasn1231@gmail.com>
2025-09-21 12:14:11 +0530

    Selection Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -17,7 +17,7 @@ public class BubbleSort {
                     temp=nums[j];
                     nums[j]=nums[j+1];
                     nums[j+1]=temp;
-                }
+                 }
             }
 //            System.out.println();
 //            for(int num : nums){
